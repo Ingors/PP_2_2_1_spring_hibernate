c6b111dcdacb3d3aee6c1ce4db7abc420d8df317
Admin-PC\Admin <itmo1652@mail.ru>
2022-09-12 00:41:57 +0300

    commit 3

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,8 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   public List<User> listUsers(String model, int series) {
+      return userDao.getUserByCar(model, series);
+   }
 }
